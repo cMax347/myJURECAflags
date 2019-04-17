7e637cd41718500bd133fd2f0330b84274460544
cMax347 <max.merte@gmail.com>
2019-04-17 15:00:56 +0200

    better working hybrid sheme

--- my_jobscript.sh
+++ my_jobscript.sh
@@ -1,57 +1,39 @@
 #!/bin/bash
-
-
 #
 #
-##### JOB SETUP #################################
+#******** JOB ALLOCATION ************************
 #SBATCH -J MEPinterp
 #SBATCH -A jiff40
 #SBATCH -N 1
-#SBATCH --ntasks=272		#(# MPI taks)
-##### #SBATCH --cpus-per-task=4 #(# openMP threads per MPI task)
+#SBATCH --ntasks=272
 #SBATCH --time=02:00:00
 #SBATCH -o log/MEPinterp-%j.out
 #SBATCH -e log/MEPinterp-%j.err
 #SBATCH -p booster
 #SBATCH --mail-user=m.merte@fz-juelich.de
 #SBATCH --mail-type=END 
-##################################################
-
-
-#
-#
-##### make sure log dir exists ##################
-$log=./log
-if[ ! -d $log ]; then
-	mkdir $log
-fi
-################################################ 
-
-
-
+################################################## 
 #
 #
 # 
-##### ENV SETUP #################################
+#******** ENV SETUP *****************************8
 #module use /usr/local/software/jurecabooster/OtherStages/
 #module load Stages/2017a 
 module load Architecture/KNL
 module load Intel IntelMPI imkl 
 ################################################
-
-
 #
 #
 #
 #
-##### JOB EXECUTION ############################
+#**** JOB EXECUTION ****************************
 #
 # MPI job
 srun -N1 --ntasks=272 mepInterp
 #
 # HYBRID job
-#export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
-#srun -N1 --ntaks=68 mepInterp
+#export OMP_NUM_THREADS=4
+#srun -N1 --ntaks=68 --cpus-per-task=4mepInterp
 
 # feel free to add additional jobs here
 # 	eg:	srun -N2 --ntasks=136 -t 10 -p booster mepInterp
