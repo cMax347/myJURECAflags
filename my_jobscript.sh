b8e7d4523f84fa9bde82acbce58edcfa5c611d94
cMax347 <max.merte@gmail.com>
2019-07-18 11:56:24 +0200

    save log in the root directory, avoids error when log folder not present

--- my_jobscript.sh
+++ my_jobscript.sh
@@ -7,8 +7,8 @@
 #SBATCH -N 1
 #SBATCH --ntasks=272
 #SBATCH --time=02:00:00
-#SBATCH -o log/MEPinterp-%j.out
-#SBATCH -e log/MEPinterp-%j.err
+#SBATCH -o log_MEPinterp-%j.out
+#SBATCH -e log_MEPinterp-%j.err
 #SBATCH -p booster
 #SBATCH --mail-user=m.merte@fz-juelich.de
 #SBATCH --mail-type=END 
@@ -33,7 +33,7 @@ srun -N1 --ntasks=272 mepInterp
 #
 # HYBRID job
 #export OMP_NUM_THREADS=4
-#srun -N1 --ntaks=68 --cpus-per-task=4mepInterp
+#srun -N1 --ntasks=68 --cpus-per-task=4mepInterp
 
 # feel free to add additional jobs here
 # 	eg:	srun -N2 --ntasks=136 -t 10 -p booster mepInterp
